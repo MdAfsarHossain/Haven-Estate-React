4fee8f650e78eef80d1bce535bedbca6f03dfcea
MdAfsarHossain <afsar291722@gmail.com>
2024-11-07 10:32:42 +0600

    Banner jsx file added

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import "./Banner.css";
+
+// import required modules
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+
+export default function Banner() {
+  return (
+    <div className="pt-10 px-10 mb-16">
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="https://images.unsplash.com/photo-1605146769289-440113cc3d00?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://images.unsplash.com/photo-1495433324511-bf8e92934d90?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://images.unsplash.com/photo-1558036117-15d82a90b9b1?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
